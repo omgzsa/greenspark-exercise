0c15c5ad3900fdaee5849da32e44415762cb3442
omgzsa <zselloandras@gmail.com>
2023-11-20 15:41:49 +0100

    widget item

---
+++ components/widget/item/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <span> This product collects </span>
+    <h4>100 plastic bottles</h4>
+    <label class="flex items-center justify-between">
+      <span> Link to Public Profile </span>
+      <input type="checkbox" />
+    </label>
+  </div>
+</template>
