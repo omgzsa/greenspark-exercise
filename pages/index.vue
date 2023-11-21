3010660c23bef617567ef52ada09e1fbe8222c71
omgzsa <zselloandras@gmail.com>
2023-11-21 21:48:46 +0100

    unused code removal

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <script setup>
-// const route = useRoute();
+const { data } = await useFetch(
+  'https://api.mocki.io/v2/016d11e8/product-widgets'
+);
 </script>
 
 <template>
-  <div>
-    <h1 class="text-gspark-green">Hello Greenspark 🌿</h1>
-    <Widget />
+  <div class="px-4 space-y-16">
+    <!-- <h1 class="text-4xl text-gspark-green">Hello Greenspark 🌿</h1> -->
+    <Widget :items="data" />
   </div>
 </template>
