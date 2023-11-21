8d3a403c7c13a0428da3e3f77a4a16a85a3f82ab
omgzsa <zselloandras@gmail.com>
2023-11-21 21:47:07 +0100

    feat: widget header w/ logo

---
+++ components/widget/item/header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center gap-4">
+    <TheLogo />
+    <div class="space-y-1 text-white">
+      <p class="widget-lead"><slot name="lead" /></p>
+      <h4 class="widget-title">
+        <slot name="title" />
+      </h4>
+    </div>
+  </div>
+</template>
