597efec281f5732e9169ffce51c4a5c136565f4c
omgzsa <zselloandras@gmail.com>
2023-11-22 14:13:03 +0100

    fix: removed static color to match theming

--- components/widget/item/header.vue
+++ components/widget/item/header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center gap-4">
     <TheLogo />
-    <div class="space-y-1 text-white">
+    <div class="space-y-1">
       <p class="widget-lead"><slot name="lead" /></p>
       <h4 class="widget-title">
         <slot name="title" />
