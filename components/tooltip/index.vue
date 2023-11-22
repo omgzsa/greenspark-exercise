fac24c0011b5f952ccdd5b9d030b56d7ece8f5ca
omgzsa <zselloandras@gmail.com>
2023-11-22 16:58:12 +0100

    fix: show/hide needs delay

--- components/tooltip/index.vue
+++ components/tooltip/index.vue
@@ -1,22 +1,35 @@
 <script setup>
-import { ref } from 'vue';
 import { useFloating, offset, flip, shift } from '@floating-ui/vue';
 
+const isVisible = ref(false);
 const reference = ref(null);
 const floating = ref(null);
 const { floatingStyles } = useFloating(reference, floating, {
   placement: 'right-end',
-  middleware: [offset(10), flip(), shift()],
+  middleware: [offset({ crossAxis: -15 }), flip(), shift()],
 });
+
+const showTooltip = () => {
+  isVisible.value = true;
+};
+
+const hideTooltip = () => {
+  isVisible.value = false;
+};
 </script>
 
 <template>
   <span
+    @mouseenter="showTooltip"
+    @mouseleave="hideTooltip"
     ref="reference"
     class="mb-2 text-[10px] border border-gspark-black w-3 h-3 flex items-center rounded-full justify-center cursor-default"
     >i</span
   >
   <div
+    @mouseenter="showTooltip"
+    @mouseleave="hideTooltip"
+    v-if="isVisible"
     ref="floating"
     :style="floatingStyles"
     class="flex flex-col px-4 py-6 space-y-2 text-center bg-white rounded-md shadow-md w-[248px]"
