7b02a77a9ab82905b19466869e0e26d388a2bfa3
omgzsa <zselloandras@gmail.com>
2023-11-22 14:13:49 +0100

    refactor: widget item to support dynamic badge colors

--- components/widget/item/index.vue
+++ components/widget/item/index.vue
@@ -9,26 +9,32 @@ const props = defineProps({
   // isLinked: Boolean,
   isActive: Boolean,
   selectedColor: String,
+  availableColors: Array,
 });
 
 const emit = defineEmits(['change']);
 
+// computed prop to show/hide the measure unit (kgs)
 const showMeasure = computed(() => {
   return props.type === 'carbon';
 });
 
+// computed prop to set the selected theme
 const selectedTheme = computed(() => {
   if (props.selectedColor === 'green') {
-    return 'bg-gspark-green';
+    return 'bg-gspark-green text-white';
   }
   if (props.selectedColor === 'blue') {
-    return 'bg-gspark-blue';
+    return 'bg-gspark-blue text-white';
   }
   if (props.selectedColor === 'beige') {
-    return 'bg-gspark-beige';
+    return 'bg-gspark-beige text-gspark-green';
   }
   if (props.selectedColor === 'black') {
-    return 'bg-gspark-black';
+    return 'bg-gspark-black text-white';
+  }
+  if (props.selectedColor === 'white') {
+    return 'bg-gspark-white text-gspark-green';
   }
 });
 
@@ -48,17 +54,37 @@ const handleInputChange = () => {
       </WidgetItemHeader>
     </div>
     <div class="py-2 space-y-2">
+      <!-- LINK TO PUBLIC PROFILE INPUT / TOOLTIP -->
       <label class="flex items-center gap-2">
-        <p>Link to public profile</p>
+        <p class="text-gspark-green">Link to public profile</p>
         <!-- <Tooltip /> -->
-        <!-- <input class="ml-auto" type="checkbox" /> -->
+        <input class="ml-auto" type="checkbox" />
       </label>
+
+      <!-- BADGE COLOUR INPUT FOR THEME CHANGE -->
       <div class="flex items-center justify-between">
-        <p>Badge colour</p>
-        <div class="w-3.5 h-3.5 border border-black bg-red-500"></div>
+        <p class="text-gspark-green">Badge colour</p>
+        <div class="flex gap-1">
+          <div
+            v-for="color in availableColors"
+            :key="color"
+            :class="{
+              'bg-gspark-green hover:bg-gspark-green/80': color === 'green',
+              'bg-gspark-blue hover:bg-gspark-blue/80': color === 'blue',
+              'bg-gspark-beige hover:bg-gspark-beige/80': color === 'beige',
+              'bg-gspark-light hover:bg-gspark-light/80': color === 'white',
+              'bg-gspark-black hover:bg-gspark-black/80': color === 'black',
+              'border-2 border-gspark-frame': color === selectedColor,
+            }"
+            @click="selectedColor = color"
+            class="w-4 h-4 transition cursor-pointer"
+          ></div>
+        </div>
       </div>
+
+      <!-- ACTIVATE BADGE INPUT -->
       <div class="flex items-center justify-between">
-        <p>Activate badge</p>
+        <p class="text-gspark-green">Activate badge</p>
         <InputToggle
           :input-id="props.id"
           v-model="badgeActive"
