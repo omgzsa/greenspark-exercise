64b589cedd2f9f5db71ea6e33159610f3d270273
omgzsa <zselloandras@gmail.com>
2023-11-23 18:34:48 +0100

    test

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,19 @@
 const { data } = await useFetch(
   'https://api.mocki.io/v2/016d11e8/product-widgets'
 );
+
+// const { data: widgets } = await useFetch('/api/widgets');
+
+// console.log(widgets);
+
+useHead({
+  title: 'Greenspark',
+});
 </script>
 
 <template>
   <div class="px-4 space-y-16">
-    <h1 class="text-4xl text-center text-gspark-green">Hello Greenspark 🌿</h1>
+    <!-- <h1 class="text-4xl text-center text-gspark-green">Hello Greenspark 🌿</h1> -->
     <Widget :items="data" />
-    <!-- <pre>
-      {{ data }}
-    </pre> -->
   </div>
 </template>
