ffbe5425a8c8cedb1dbe9e5bad949ed8c5e5aaee
omgzsa <zselloandras@gmail.com>
2023-11-23 14:57:45 +0100

    feat: improved tooltip behavior

--- components/tooltip/index.vue
+++ components/tooltip/index.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { useFloating, offset, flip, shift } from '@floating-ui/vue';
 
-const isVisible = ref(false);
+const isHovered = ref(false);
+const isTooltipHovered = ref(false);
+
 const reference = ref(null);
 const floating = ref(null);
 const { floatingStyles } = useFloating(reference, floating, {
@@ -10,11 +12,26 @@ const { floatingStyles } = useFloating(reference, floating, {
 });
 
 const showTooltip = () => {
-  isVisible.value = true;
+  isHovered.value = true;
 };
 
 const hideTooltip = () => {
-  isVisible.value = false;
+  if (!isTooltipHovered.value) {
+    setTimeout(() => {
+      isHovered.value = false;
+    }, 300);
+  }
+};
+
+const tooltipHovered = () => {
+  isTooltipHovered.value = true;
+};
+
+const tooltipLeft = () => {
+  setTimeout(() => {
+    isTooltipHovered.value = false;
+    isHovered.value = false;
+  }, 200);
 };
 </script>
 
@@ -23,22 +40,40 @@ const hideTooltip = () => {
     @mouseenter="showTooltip"
     @mouseleave="hideTooltip"
     ref="reference"
-    class="mb-2 text-[10px] border border-gspark-black w-3 h-3 flex items-center rounded-full justify-center cursor-default"
+    class="mb-2 text-[10px] border border-gspark-green text-gspark-green w-3 h-3 flex items-center rounded-full justify-center cursor-default transition hover:scale-110 hover:font-semibold"
     >i</span
   >
-  <div
-    @mouseenter="showTooltip"
-    @mouseleave="hideTooltip"
-    v-if="isVisible"
-    ref="floating"
-    :style="floatingStyles"
-    class="flex flex-col px-4 py-6 space-y-2 text-center bg-white rounded-md shadow-md w-[248px]"
-  >
-    <p>
-      This widget links directly to your public profile so that you can easily
-      share your impact with your customers. Turn it off here if you do not want
-      the badge to link to it.
-    </p>
-    <a class="font-semibold text-gspark-green" href="/">View Public Profile</a>
-  </div>
+  <Transition name="fade">
+    <div
+      @mouseenter="tooltipHovered"
+      @mouseleave="tooltipLeft"
+      v-if="isHovered || isTooltipHovered"
+      ref="floating"
+      :style="floatingStyles"
+      class="flex flex-col px-4 py-6 space-y-2 text-center bg-white rounded-md shadow-md w-[248px]"
+    >
+      <p>
+        This widget links directly to your public profile so that you can easily
+        share your impact with your customers. Turn it off here if you do not
+        want the badge to link to it.
+      </p>
+      <a
+        class="font-semibold transition text-gspark-green hover:text-gspark-green/80"
+        href="/"
+        >View Public Profile</a
+      >
+    </div>
+  </Transition>
 </template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.15s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
